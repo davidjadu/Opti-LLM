60d89bf9132ec8275b1981154611f404b6cd172e
RedaELMARRAKCHI <reda.elmarrakchi@gmail.com>
2024-12-18 15:25:04 +0100

    update

--- appli/src/main/java/com/example/springiapromptdemo/entities/LLMResponse.java
+++ appli/src/main/java/com/example/springiapromptdemo/entities/LLMResponse.java
@@ -13,6 +13,7 @@ public class LLMResponse {
     private Long id;
     @ManyToOne
     private LLMPrompt prompt;
+    @OneToOne
     private DataSet dataSet;
     private Float score;
     @Temporal(TemporalType.TIMESTAMP)
